9286138c065099bd6fab2b4c2d49435f98ba2256
carine_cruz <carinedenisecruz@gmail.com>
2022-04-14 00:55:44 +0800

    Added components based on tutorial

---
+++ src/data.js
@@ -0,0 +1,89 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/82RfcpB/ussama-azam-5-Ic-EBm-SOQq0-unsplash.jpg",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/FqzbCX7/pexels-polina-tankilevitch-6073545.jpg",
+        title: "AUTUMN COLLECTION",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd"
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/Yhjd93p/pexels-spencer-selover-428338.jpg",
+        title: "POPULAR COLLECTION",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fbf0f4"
+    }
+]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/cLFxm3T/pexels-ron-lach-9604182-1.jpg",
+        title: "SHIRT STYLE!"
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/sw6kzDW/pexels-polina-tankilevitch-6630811-1.jpg",
+        title: "LOUNGEWEAR LOVE"
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/jD2xt5q/pexels-thirdman-8053906.jpg",
+        title: "DREAMY DRESSES"
+    }
+]
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/Lh2DvS4/ryan-hoffman-6-Nub980b-I3-I-unsplash.jpg"
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/G22Jd7n/raquel-gambin-k-S3-Yk-Vtf85-U-unsplash.jpg"
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/CHpNMxD/pexels-tu-n-ki-t-jr-1308885.jpg"
+    },
+    {
+        id: 4,
+        img: "https://i.ibb.co/5kN8vqW/pexels-olena-ukraine-1021709.jpg"
+    },
+    {
+        id: 5,
+        img: "https://i.ibb.co/RCDtNgL/pexels-pixabay-532556.jpg"
+    },
+    {
+        id: 6,
+        img: "https://i.ibb.co/sgWZmbv/pexels-anj-namoro-2850487.jpg"
+    },
+    {
+        id: 7,
+        img: "https://i.ibb.co/hmdNrLm/pexels-anastasia-zhenina-63448.jpg"
+    },
+    {
+        id: 8,
+        img: "https://i.ibb.co/XXDfrN9/pexels-cottonbro-4937448.jpg"
+    },
+    {
+        id: 9,
+        img: "https://i.ibb.co/6ydjF4Y/pexels-cottonbro-4066293.jpg"
+    },
+    {
+        id: 10,
+        img: "https://i.ibb.co/68ktFvM/pexels-jos-luis-photographer-2522681.jpg"
+    },
+    {
+        id: 11,
+        img: "https://i.ibb.co/TTSZty2/pexels-mart-production-9558577.jpg"
+    }
+]
+
